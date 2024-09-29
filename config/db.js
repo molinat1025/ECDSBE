972245fb32ec2e527c123d59a106899672b1fb5e
Tony Molina <pgraduacion.am2@gmail.com>
2024-09-28 23:36:09 -0600

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connectDB = async () => {
+    const uri = process.env.MONGODB_URI; // Usar la variable de entorno para obtener el URI
+
+    await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => console.log("DB Connected"))
+      .catch((error) => console.error("DB Connection Error: ", error));
+}
