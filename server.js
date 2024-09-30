388e038de1259c8240453d7d0caba8e8e129497b
Tony Molina <pgraduacion.am2@gmail.com>
2024-09-29 23:32:35 -0600

    Heroku cors modified 2

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 4000;  // Heroku usará process.env.PORT, para 
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5174',  // Permitir solicitudes solo desde localhost:5174
+    origin: '*',  // Permitir solicitudes solo desde localhost:5174
     credentials: true  // Si necesitas enviar cookies o credenciales entre los dominios
   }));
 
