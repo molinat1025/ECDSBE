f2e7b194f808a0a3c251d894670e99947ab9a8c2
Tony Molina <pgraduacion.am2@gmail.com>
2024-10-03 23:33:31 -0600

    Agregar dirección en cors en Heroku

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 4000;  // Heroku usará process.env.PORT, para desarrollo local usa 4000
 // Lista de orígenes permitidos
 const allowedOrigins = [
+    'http://localhost:5173',
     'http://localhost:5174', // Frontend administrativo en localhost
     'https://dsecommerce-pg2-b83249af3924.herokuapp.com' // Frontend de clientes en Heroku
   ];
